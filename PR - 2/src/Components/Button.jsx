b67c11927dded49bfb8817f1e72b8fd1817afa82
ShreyaRajeshbhaipal <shreya0411@gmail.com>
2026-01-27 21:29:15 +0530

    pr-2

---
+++ PR - 2/src/Components/Button.jsx
@@ -0,0 +1,13 @@
+const Button = ({ name, count, handleClick }) => {
+  return (
+    <button
+      className="action-btn"
+      onClick={handleClick}
+      disabled={count <= 0 && name === "DEC"}
+    >
+      {name}
+    </button>
+  );
+};
+
+export default Button;
